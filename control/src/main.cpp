28caff2908f0f128fb75765a7945682f6774f3c9
Gautam Dayal <gautam.dayal@yahoo.com>
2024-03-30 19:32:28 -0500

    constexpr instead of const

--- control/src/main.cpp
+++ control/src/main.cpp
@@ -1,21 +1,21 @@
 #include <Arduino.h>
 
 // Pin assignments
-const int M1_IN1 =  1; 
-const int M1_IN2 =  2; 
+constexpr int M1_IN1 =  1; 
+constexpr int M1_IN2 =  2; 
 const int M1_ENA =  0; 
 
-const int M2_IN1 =  3; 
-const int M2_IN2 =  4; 
-const int M2_ENA =  5; 
+constexpr int M2_IN1 =  3; 
+constexpr int M2_IN2 =  4; 
+constexpr int M2_ENA =  5; 
 
-const int M3_IN1 =  8; 
-const int M3_IN2 =  9; 
-const int M3_ENA =  7; 
+constexpr int M3_IN1 =  8; 
+constexpr int M3_IN2 =  9; 
+constexpr int M3_ENA =  7; 
 
-const int M4_IN1 =  11;
-const int M4_IN2 =  10;
-const int M4_ENA =  12; 
+constexpr int M4_IN1 =  11;
+constexpr int M4_IN2 =  10;
+constexpr int M4_ENA =  12; 
 
 // Struct to store each motor's pins in a cleaner manner
 struct MotorPins{
