a16da59e2b246ca0929834d9fe0cf7d8b34f433b
Gautam Dayal <gautam.dayal@yahoo.com>
2024-03-31 15:33:24 -0500

    Skeleton for main software loop, will work on pyserial setup later today

---
+++ main.py
@@ -0,0 +1,14 @@
+import serial
+import perception
+
+# Initialize things
+
+# Main loop
+while 1:
+    # Run perception loop
+
+    # Run planner
+
+    # Go through plan and send motor commands for appropriate durations
+
+    pass
