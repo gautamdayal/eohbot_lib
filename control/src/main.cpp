3cb7419c6afae6384e82a541753975f94eb9e0b2
Gautam Dayal <gautam.dayal@yahoo.com>
2024-04-23 19:48:29 -0500

    I think it works question mark

--- control/src/main.cpp
+++ control/src/main.cpp
@@ -1,10 +1,19 @@
 #include <Arduino.h>
+#include <Wire.h>
 #include <drivetrain_hal.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <Adafruit_Sensor.h>
 
-// Pin assignments
+/*
+  Compile-time options
+  - MOTORS: Preset motor movement sequences for demos and hardware tests.
+  - SERIAL: When you want the autonomy stack to work. Accepts direction via serial.
+*/
 // #define MOTORS
 #define SERIAL
 
+// Pin assignments
 MotorPins motor_1{M1_ENA, M1_IN1, M1_IN2};
 MotorPins motor_2{M2_ENA, M2_IN1, M2_IN2};
 MotorPins motor_3{M3_ENA, M3_IN1, M3_IN2};
@@ -12,7 +21,9 @@ MotorPins motor_4{M4_ENA, M4_IN1, M4_IN2};
 
 Robot rat{motor_1, motor_2, motor_3, motor_4};
 
-// the setup function runs once when you press reset or power the board
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+
+// The setup function runs once when you press reset or power the board
 void setup() {
   // Motor 1 Initialization
   pinMode(M1_IN1, OUTPUT);
@@ -37,14 +48,21 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(9600);
-  delay(5000);
 
+  if (!bno.begin()) {
+    Serial.println("No BNO detected");
+  }
+  delay(5000);
+  bno.setExtCrystalUse(true);
 }
 
 int direction = 999;
 
 // the loop function runs over and over again forever
 void loop() {
+  sensors_event_t event;
+  bno.getEvent(&event);
+  Serial.println((float)event.acceleration.x);
 
   #ifdef MOTORS
   for (int i = 0; i < 90; i++) {
