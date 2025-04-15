9487a41f9cbf376a5f4ce02b4362f0ca01fe7456
gautamdayal <gautam.dayal@yahoo.com>
2025-04-14 22:37:54 -0500

    Added calibration values for controller

---
+++ gamepad/calibration.py
@@ -0,0 +1,17 @@
+joystick_l_hor_right = 32767 # ABS_X
+joystick_l_hor_center = 0
+joystick_l_hor_left = -32767
+joystick_l_vert_top = -327678 # ABS_Y 
+joystick_l_vert_center = -2
+joystick_l_vert_bottom = 32767
+
+joystick_r_hor_right = 32767 # ABS_RX
+joystick_r_hor_center = 0
+joystick_r_hor_left = -32768
+joystick_r_vert_top = -327678 # ABS_RY
+joystick_r_vert_center = -2
+joystick_r_vert_bottom = 32767
+
+left_trigger_down = 255 # ABS_Z
+right_trigger_down = 255 # ABS_RZ
+
